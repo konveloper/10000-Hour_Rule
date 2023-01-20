753fddce4aadd07ad6054336f58c5c9fe85e6c33
konveloper <konveloper@gmail.com>
2023-01-20 23:59:49 +0900

    feat : 계산 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-// js file
+const startBtn = document.querySelector('.btn-start');
+const goBtn = document.querySelector('.btn-go');
+const shareBtn = document.querySelector('.btn-share');
+const closeBtn = document.querySelector('.btn-close');
+const loading = document.querySelector('.container-result-loading');
+const result = document.querySelector('.container-result');
+const modal = document.querySelector('#modal');
+
+function calculator() {
+  const jobValue = document.querySelector('#value-job');
+  let timeValue = document.querySelector('#value-time');
+  let timeValue_int = Number(timeValue.value);
+
+  const jobResult = document.querySelector('.result-job');
+  const timeResult = document.querySelector('.result-time');
+
+  if (jobValue.value == '') {
+    alert('입력되지 않았습니다.');
+    jobValue.focus();
+    return false;
+  } else if (timeValue.value == '') {
+    alert('입력되지 않았습니다.');
+    timeValue.focus();
+    return false;
+  } else if (timeValue_int > 24) {
+    alert('24 이하의 값을 입력해주세요.');
+    return false;
+  }
+
+  result.style.display = 'none';
+  loading.style.display = 'flex';
+
+  setTimeout(function () {
+    loading.style.display = 'none';
+    result.style.display = 'flex';
+    jobResult.innerText = jobValue.value;
+    timeResult.innerText = parseInt(10000 / timeValue_int, 10);
+  }, 1800);
+}
+
+function openModal() {}
+
+function closeModal() {}
+
+function copyUrl() {}
+
+startBtn.addEventListener('click', calculator);
+goBtn.addEventListener('click', openModal);
+shareBtn.addEventListener('click', copyUrl);
+closeBtn.addEventListener('click', closeModal);
